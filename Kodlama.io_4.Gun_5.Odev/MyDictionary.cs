315b1c9fc89da50efabfc9f8f56efc7948cb89ba
Serkan Turgay <turgay.serkan@outlook.com>
2021-02-06 14:40:44 +0300

    Initial commit

---
+++ Kodlama.io_4.Gun_5.Odev/MyDictionary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kodlama.io_4.Gun_5.Odev
+{
+    class MyDictionary<Tno,Tname>
+    {
+        Tno[] No;
+        Tname[] Names;
+        public MyDictionary()
+        {
+            No =new Tno[0];
+            Names = new Tname[0];
+        }
+
+        public void Add(Tno no, Tname name)
+        {
+
+            Tno[] tempNo = No;
+            Tname[] tempName = Names;
+            No = new Tno[tempNo.Length + 1];
+            Names = new Tname[tempName.Length + 1];
+            for (int i = 0; i < tempNo.Length; i++)
+            {
+                No[i] = tempNo[i];
+            }
+            No[No.Length - 1] = no;
+            for (int i = 0; i < tempName.Length; i++)
+            {
+                Names[i] = tempName[i];
+            }
+            Names[Names.Length - 1] = name;
+        }
+
+    }
+}
---
+++ Kodlama.io_4.Gun_5.Odev/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Kodlama.io_4.Gun_5.Odev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> students = new MyDictionary<int, string>();
+            students.Add(132, "Ahmet");
+            students.Add(152, "Mehmet");
+            students.Add(456, "Salih");
+            students.Add(476, "Hasan");
+            students.Add(898, "Kemal");
+            students.Add(343, "Serkan");
+
+            Console.WriteLine(students);
+
+
+        }
+    }
+}
